3901ab3633a3a98986cdfb5c140ca196b9a39373
regnna <ritmchats@gmail.com>
2023-08-16 12:28:41 +0530

    5thcommit

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -7,6 +7,6 @@ const secret=process.env.NEXTAUTH_SECRET
 
 export async function GET(req:NextRequest){
     const token=await getToken({req,secret,raw:true});
-
+    
     return NextResponse.json({token},{status:200});
 }
